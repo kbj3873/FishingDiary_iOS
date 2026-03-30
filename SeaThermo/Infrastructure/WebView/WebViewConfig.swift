49019f07feca4ed0e0efc9fca85a097ad78c71fa
Kim Byeong Joon <>
2026-03-30 15:54:19 +0900

    260330 @ kim b j : 오픈소스 라이선스 url 변경 적용

--- SeaThermo/Infrastructure/WebView/WebViewConfig.swift
+++ SeaThermo/Infrastructure/WebView/WebViewConfig.swift
@@ -8,7 +8,7 @@ import Foundation
 /// 온바다 서버 웹뷰 페이지 정의
 enum WebPage: String, Identifiable {
     case notices  = "/notices-page/"  // 공지사항
-    case licenses = "/licenses/"      // 오픈소스 라이선스
+    case licenses = "/licenses/iOS"      // 오픈소스 라이선스
     
     var id: String { rawValue }
     var path: String { rawValue }
